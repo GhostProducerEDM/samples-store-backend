8bfd452c595e7cbbe16c47c4e2ad991be31fdd49
GhostProducerEDM <Yourghostproduceredm@gmail.com>
2026-04-20 14:46:16 +0100

    Delete migrations directory

--- migrations/003_fk_constraints.sql
+++
@@ -1,9 +0,0 @@
--- Add foreign key from user_likes to samples so Supabase relational queries work
-ALTER TABLE user_likes
-  ADD CONSTRAINT IF NOT EXISTS user_likes_sample_id_fkey
-  FOREIGN KEY (sample_id) REFERENCES samples(id) ON DELETE CASCADE;
-
--- Add foreign key from user_downloads to samples (same reason)
-ALTER TABLE user_downloads
-  ADD CONSTRAINT IF NOT EXISTS user_downloads_sample_id_fkey
-  FOREIGN KEY (sample_id) REFERENCES samples(id) ON DELETE CASCADE;
